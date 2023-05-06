3a68c59448e2f16ea5aed4057ba2523e1dd74db2
vprint <valentin.printemps@gmail.com>
2023-05-06 15:13:47 +0200

    Modification de la barre latérale
    Ajout d'une icone layer et positionnement de l'icone.

--- widget/LeftSidebar.js
+++ widget/LeftSidebar.js
@@ -1,5 +1,6 @@
 import { SidePanel } from 'ol-side-panel';
-import EditBar from './Edition/EditBar';
+import EditionTool from './Edition/EditionTool';
+import '@fortawesome/fontawesome-free/css/all.css';
 
 /**
  * @module widget
@@ -9,6 +10,7 @@ class LeftSidebar {
   Sidebar;
   layersPane;
   constructor(map) {
+
     // Creation sidebar
     this.Sidebar = new SidePanel
     map.addControl(this.Sidebar);
@@ -17,10 +19,11 @@ class LeftSidebar {
     this.layersPane = this.Sidebar.definePane({
       paneId: 'layers',
       name: "Layers",
-      icon: 'Y'
+      icon: '<i class="fa-sharp fa-solid fa-layer-group" style="position:relative;left:-3px;top:-6px"</i>'
     });
 
-    new EditBar(this.layersPane)
+    // Ajout de l'outil de sélection et de création
+    new EditionTool(this.layersPane)
   }
 }
 
